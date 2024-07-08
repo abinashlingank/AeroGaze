9d95e55a4c55c79e9e0d0fea66620e49eb387bfc
Jaya prakash L <123235911+coldheartboy@users.noreply.github.com>
2024-07-08 20:43:25 +0530

    Add files via upload

---
+++ Rasyolo/detect1.py
@@ -0,0 +1,12 @@
+from Rasyolo.getDistance import GetDistance
+def run(model, image, save, conf_thres):
+    results = model.predict(source=image, save=save)
+    boxes = results[0].boxes.xyxy.cpu().tolist()
+    clss = results[0].boxes.cls.cpu().tolist()
+    confi = results[0].boxes.conf.cpu().tolist()
+    decision = (0,0,0,0,'')
+    for box, cls, conf in zip(boxes, clss, confi):
+        bbox_center = ((box[0]+box[2])/2, (box[1]+box[3])/2)
+        cls = model.model.names[int(cls)]
+        decision = GetDistance(bbox_center)
+    return decision
