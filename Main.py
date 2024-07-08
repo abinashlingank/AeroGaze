59fcf68f5573085fc02495e7aefaebfe3f57ef71
Jaya prakash L <123235911+coldheartboy@users.noreply.github.com>
2024-07-08 16:48:36 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,12 @@ from picamera2 import Picamera2
 import uuid
 import time
 import os
-from Rasyolo.detect import run
+from Rasyolo.detect1 import run
+from ultralytics import YOLO
+
+
+#load the model
+model = YOLO(weight)
 
 try:
     # Making Connection
@@ -17,13 +22,18 @@ try:
     print("Destination coordinates: ", destlat, destlong)
     #destlat, destlong = 12.971730433350809,80.04374350577795
 
+    print('Change vehicle mode')
+    vehicle.mode = VehicleMode('GUIDED')
+    time.sleep(2)
+    print('Vehicle mode: ', vehicle.mode)
+
     # Taking off
     arm_and_takeoff(vehicle, altitude)
 
     # Changing to Guided mode
     #if vehicle.mode.name != "GUIDED":
     vehicle.mode = VehicleMode("GUIDED")
-    print("Vehicle mode changed...")
+
     # Going to Destination
     dest_location = LocationGlobalRelative(destlat, destlong, altitude)
     vehicle.simple_goto(dest_location, groundspeed=speed)
@@ -37,10 +47,13 @@ try:
         capture_image("image.jpg", camera)
         print("After capture image")
         # Running Yolo
-        decision = run(weights=weight, source="image.jpg", conf_thres=conf)
-        #decision = (0, 101.5, 0, 86.25, 'Quadrant 3')
+        decision = run(weight, 'image.jpg', True, conf_thres=0.8)
+        if not any(decision):
+            print('helipad not detected')
+            continue
 
         # Moving to helipad
+        print('Drone is in quadrant', decision[4])
         print("Taken by AI....")
         forwardLocation = get_location_metres(vehicle.location.global_relative_frame, decision[0], vehicle.heading)
         backwardLocation = get_location_metres(vehicle.location.global_relative_frame, decision[1], vehicle.heading)
