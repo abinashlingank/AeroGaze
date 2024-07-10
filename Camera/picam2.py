73f12ed37a49239b24022ee6ea6e1eae1756e2f4
abinashlingank <abinashlingank@gmail.com>
2024-07-10 16:54:37 +0530

    Updated picam2.py for configure image resolution

--- Camera/picam2.py
+++ Camera/picam2.py
@@ -3,6 +3,8 @@ import time
 from picamera2 import Picamera2
 
 camera = Picamera2()
+camera_config = camera.create_still_configuration(main={"size": (1920, 1080)})
+camera.configure(camera_config)
 start_time = time.time()
 i = 1
 while time.time() - start_time < 100:
